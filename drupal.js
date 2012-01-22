7ce741a6440608ad6841b55e20fea207bccec334
Eirik S. Morland <eirik@morland.no>
2012-01-22 13:01:39 +0100

    works. now with universal prompts and posting nodes!

--- drupal.js
+++ drupal.js
@@ -1,28 +1,105 @@
-function login() {
-	var pass = $('#password').val();
-	var user = $('#user').val();
-	var url = url + '/user/login.json'
-	var data = {
-		"username": user,
-		"password": pass
+/**
+ * Log in to a Drupal site.
+ *
+ * @param data
+ *   Array containing url, pass and user. URL should be to endpoint.
+ * @param successFunction
+ *   A callback function invoked when the login was successful.
+ * @param errorFunction
+ *   A callback function invoked when the login was unsuccessful.
+ */
+
+function drupalServicesLogin(data, successFunction, errorFunction) {
+	if (typeof successFunction == 'undefined') {
+		var successFunction = drupalServicesAwesome;
+	};
+	if (typeof errorFunction == 'undefined') {
+		var errorFunction = drupalServicesDarned;
+	};
+	var url = data['url'] + '/user/login.json'
+	var post = {
+		"username": data['user'],
+		"password": data['pass']
 	}
-	url = $('#enpoint').val() + '/user/login.json';
 	$.ajax({
         url: url,
         type: "POST",
-		data: data,
-		success: function() {
-			alert('Sucessfully logged in');
+		data: post,
+		success: function(data) {
+			successFunction(data);
+		},
+		error: function(data) {
+			errorFunction(data);
+		}
+	});
+}
+
+/**
+ * Log out of a Drupal site.
+ *
+ * @param url
+ *   URL to services endpoint at Drupal instance.
+ * @param successFunction
+ *   A callback function invoked when the logout was successful.
+ * @param errorFunction
+ *   A callback function invoked when the logout was unsuccessful.
+ */
+function drupalServicesLogout(url, successFunction, errorFunction) {
+	if (typeof successFunction == 'undefined') {
+		var successFunction = drupalServicesAwesome;
+	};
+	if (typeof errorFunction == 'undefined') {
+		var errorFunction = drupalServicesDarned;
+	};
+	var url = url + '/user/logout.json';
+	$.ajax({
+        url: url,
+        type: "POST",
+		success: function(data) {
+			successFunction(data);
+		},
+		error: function(data) {
+			errorFunction(data);
 		}
 	})
 }
-function logout(url) {
-	var url = url + '/user/logout.json'
+
+/**
+ * Post a node to a Drupal site.
+ *
+ * @param node
+ *   A populated node object.
+ * @param url
+ *   URL to services endpoint at Drupal instance.
+ * @param successFunction
+ *   A callback function invoked when the login was successful.
+ * @param errorFunction
+ *   A callback function invoked when the login was unsuccessful.
+ */
+function drupalServicesPostNode(node, url, successFunction, errorFunction) {
+	if (typeof successFunction == 'undefined') {
+		var successFunction = drupalServicesAwesome;
+	};
+	if (typeof errorFunction == 'undefined') {
+		var errorFunction = drupalServicesDarned;
+	};
+	var url = url + '/node.json';
 	$.ajax({
         url: url,
         type: "POST",
-		success: function() {
-			alert('Sucessfully logged out');
+		data: node,
+		success: function(data) {
+			successFunction(data);
+		},
+		error: function(data) {
+			errorFunction(data);
 		}
-	})
+	});
+}
+
+function drupalServicesAwesome(data) {
+	console.log(data);
+}
+function drupalServicesDarned(data) {
+	console.log(data);
 }
